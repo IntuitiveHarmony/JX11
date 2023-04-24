58239f0a1e9a43f0835614f69fd208cac7d2627c
Intuitive Harmony <j.horst77@gmail.com>
2023-04-23 23:30:21 -0600

    Parameters - the rest

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -30,7 +30,7 @@ void JX11AudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!\n\nThis is one of my first steps\n into making Audio and MIDI plug ins\n\nAbove is the code that grabs each incoming 3 byte MIDI message from the host\n\nTo the right is data from my MIDI controller\nbeing captured as I play it live\n\nEach line represents information such as:\nnote on/off, note value, velocity, channel and timestamp\n\nThanks to Matthijs Hollemans and his book\nCode your Own Synth Plug-ins\n\nHow can I be learning so much\n and still have no idea what I'm doing? :p", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void JX11AudioProcessorEditor::resized()
