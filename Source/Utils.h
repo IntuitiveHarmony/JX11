e63cb2d2534c105ab0a0215ce536bae412d92dfa
Intuitive Harmony <j.horst77@gmail.com>
2023-05-22 09:51:22 -0600

    pointers for parameters

--- Source/Utils.h
+++ Source/Utils.h
@@ -46,3 +46,9 @@ inline void protectYourEars(float* buffer, int sampleCount)
         }
     }
 }
+
+template<typename T> inline static void castParameter(juce::AudioProcessorValueTreeState& apvts, const juce::ParameterID& id, T& destination)
+{
+    destination = dynamic_cast<T>(apvts.getParameter(id.getParamID()));
+    jassert(destination); // parameter doesn't exist or wrong type
+}
